07981bb7e0a95cb9013a82d068e1cab40eccec96
Jerome Wu <jeromewus@gmail.com>
2018-02-05 11:15:27 +0800

    feat: add command check & fix bugs

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ main(){
   timestamp=$(date +%s)
   basePluginDir=~/.vim/bundle
 
+  echo "Check dependencies"
+  for cmd in git cmake python node
+  do
+    command -v $cmd > /dev/null 2>&1 || { echo >&2 "$cmd is required for this script. Aborting"; exit 1; }
+  done
+
   echo "Backup the existing .vimrc file and .vim folder"
   mv ~/.vimrc ~/.vimrc.${timestamp}
   mv ~/.vim ~/.vim.${timestamp}
@@ -13,21 +19,21 @@ main(){
 
   echo "Copy other config files"
   cp ./.tern-project ~/.tern-project
-  cp -r ./UltiSnips ~/.vim/UltiSnips
+  cp -r ./UltiSnips ~/.vim/
 
   echo "Install pathogen.vim"
   mkdir -p ~/.vim/autoload ${basePluginDir}
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
   echo "Install plugins"
-  git clone https://github.com/nanotech/jellybeans.vim ${basePluginDir}/jellybeans.vim
-  git clone https://github.com/scrooloose/nerdtree ${basePluginDir}/nerdtree
-  git clone https://github.com/scrooloose/syntastic ${basePluginDir}/syntastic
-  git clone https://github.com/pangloss/vim-javascript ${basePluginDir}/vim-javascript
-  git clone https://github.com/mxw/vim-jsx ${basePluginDir}/vim-jsx
-  git clone https://github.com/heavenshell/vim-jsdoc ${basePluginDir}/vim-jsdoc
-  git clone https://github.com/Valloric/YouCompleteMe ${basePluginDir}/YouCompleteMe
-  git clone https://github.com/SirVer/ultisnips ${basePluginDir}/ultisnips
+  git clone --depth 1 https://github.com/nanotech/jellybeans.vim ${basePluginDir}/jellybeans.vim
+  git clone --depth 1 https://github.com/scrooloose/nerdtree ${basePluginDir}/nerdtree
+  git clone --depth 1 https://github.com/scrooloose/syntastic ${basePluginDir}/syntastic
+  git clone --depth 1 https://github.com/pangloss/vim-javascript ${basePluginDir}/vim-javascript
+  git clone --depth 1 https://github.com/mxw/vim-jsx ${basePluginDir}/vim-jsx
+  git clone --depth 1 https://github.com/heavenshell/vim-jsdoc ${basePluginDir}/vim-jsdoc
+  git clone --depth 1 https://github.com/Valloric/YouCompleteMe ${basePluginDir}/YouCompleteMe
+  git clone --depth 1 https://github.com/SirVer/ultisnips ${basePluginDir}/ultisnips
 
   echo "Install YouCompleteMe"
   cd ~/.vim/bundle/YouCompleteMe
